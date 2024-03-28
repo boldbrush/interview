496864c113e9c4378aebe3bac256c6dd2ecbca1a
Ricardo Perez <ricpp87@gmail.com>
2024-03-28 10:15:10 -0500

    Set global timeout options

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Date::use(CarbonImmutable::class);
+
+        $this->globalHttpClientConfiguration();
+    }
+
+    private function globalHttpClientConfiguration(): void
+    {
+        Http::globalOptions([
+            'timeout' => 3.0,
+            'connect_timeout' => 1.0,
+        ]);
+
+        Event::listen(CommandStarting::class, function (CommandStarting $event) {
+            if ($event->command == 'db:seed') {
+                // override defaults when seeding database
+                Http::globalOptions([
+                    'timeout' => 10,
+                    'connect_timeout' => 1.0,
+                ]);
+            }
+        });
     }
 }
